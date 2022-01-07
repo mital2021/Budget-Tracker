1c186075a680957ce4e0d3731559b8901b97ad99
Mital <mitalg0717@gmail.com>
2022-01-07 15:27:40 -0500

    added manifest file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,22 @@
-const APP_PREFIX = 'BudgetTracker-';     
+const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CACHE = [
-  './index.html',
-  './css/styles.css',
-  './js/index.js',
-  './js/idb.js',
-  './manifest.json',
-  './icons/icon-512x512.png',
-  './icons/icon-384x384.png',
-  './icons/icon-192x192.png',
-  './icons/icon-152x152.png',
-  './icons/icon-144x144.png',
-  './icons/icon-128x128.png',
-  './icons/icon-96x96.png',
-  './icons/icon-72x72.png'
- 
+    './',
+    './index.html',
+    './css/styles.css',
+    './js/index.js',
+    './js/idb.js',
+    './manifest.json',
+    './icons/icon-512x512.png',
+    './icons/icon-384x384.png',
+    './icons/icon-192x192.png',
+    './icons/icon-152x152.png',
+    './icons/icon-144x144.png',
+    './icons/icon-128x128.png',
+    './icons/icon-96x96.png',
+    './icons/icon-72x72.png'
 ];
 
 // Respond with cached resources
@@ -70,3 +71,4 @@ self.addEventListener('activate', function(e) {
     })
   );
 });
+   
